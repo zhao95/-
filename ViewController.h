32a2a8350135183cfaa0c39b668f5aa6551e7e41
泽熙 <zhao95@outlook.com>
2016-06-23 18:39:28 +0800

    非主流即时通讯基础框架
    非主流即时通讯基础框架

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  随意练习
+//
+//  Created by 赵恩峰 on 16/6/23.
+//  Copyright © 2016年 赵恩峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  随意练习
+//
+//  Created by 赵恩峰 on 16/6/23.
+//  Copyright © 2016年 赵恩峰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
